6b31fc5ee135677bcd62d03546ea5fe023377bf7
loostro <loostro@gmail.com>
2013-06-28 11:01:44 +0100

    Load form service definitions

--- DependencyInjection/AvocodeFormExtensionsExtension.php
+++ DependencyInjection/AvocodeFormExtensionsExtension.php
@@ -4,6 +4,7 @@ namespace Avocode\FormExtensionsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -46,6 +47,46 @@ class AvocodeFormExtensionsExtension extends Extension
         
         if (!$config['use_genemu_form']) {
             $loader->load('form.xml');
+        }
+        
+        $this->loadBootstrapCollectionTypes($container);
+        $this->loadSelect2Types($container);
+    }
+    
+    private function loadBootstrapCollectionTypes(ContainerBuilder $container)
+    {
+        $serviceId = 'avocode.form.extensions.type.bootstrap_collection';
+        
+        $bootstrapCollectionTypes = array('fieldset', 'table');
+        
+        foreach ($bootstrapCollectionTypes as $type) {
+            $typeDef = new DefinitionDecorator($serviceId);
+            $typeDef
+                ->addArgument($type)
+                ->addTag('form.type', array('alias' => 'collection_'.$type))
+            ;
+
+            $container->setDefinition($serviceId.'.'.$type, $typeDef);
+        }
+    }
+    
+    private function loadSelect2Types(ContainerBuilder $container)
+    {
+        $serviceId = 'avocode.form.extensions.type.select2';
+        
+        $select2types = array(
+            'choice', 'language', 'country', 'timezone', 
+            'locale', 'entity', 'document', 'model', 'hidden'
+        );
+        
+        foreach ($select2types as $type) {
+            $typeDef = new DefinitionDecorator($serviceId);
+            $typeDef
+                ->addArgument($type)
+                ->addTag('form.type', array('alias' => 'select2_'.$type))
+            ;
+
+            $container->setDefinition($serviceId.'.'.$type, $typeDef);
         }
     }
 }
