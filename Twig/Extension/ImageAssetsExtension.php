f814446810bb2844d770605f17035f57b260ac4f
loostro <loostro@gmail.com>
2013-07-13 11:28:02 +0100

    Update

--- Twig/Extension/ImageAssetsExtension.php
+++ Twig/Extension/ImageAssetsExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * 
  * @author Piotr Gołębiewski <loostro@gmail.com>
  */
-class ImageFilterExtension extends \Twig_Extension
+class ImageAssetsExtension extends \Twig_Extension
 {
     protected $container;
 
@@ -26,38 +26,79 @@ class ImageFilterExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'image_filter' => new \Twig_Function_Method($this, 'filter'),
+            'image_asset'   =>  new \Twig_Function_Method($this, 'asset'),
         );
     }
 
+    public function getFilters()
+    {
+        return array(
+            'image_filter'  =>  new \Twig_Filter_Method($this, 'filter'),
+        );
+    }
+    
     /**
      * Gets the browser path for the image and filter to apply.
+     *
+     * @return string The public path.
+     */
+    public function asset($object, $field)
+    {
+        $params = func_get_args();
+        
+        if ('vich_uploader' === $this->getAssetProvider()) {
+            $ext = $this->container->get('vich_uploader.twig.extension.uploader'); 
+            return call_user_func_array(array($ext, "asset"), $params);
+        }
+        
+        // In case no asset provider is used we expect object to have
+        // a special method returning file's path
+        $getter = "get".ucfirst($field)."Path";
+        
+        return $object->$getter();
+    }
+
+    /**
+     * Gets the browser path for the image and filter to apply
      *
      * @return string
      */
     public function filter()
     {
         $params = func_get_args();
+        $path = $params[0];
         
-        if ('liip_imagine' === $this->getThumbnailGenerator()) {
+        if ('liip_imagine' === $this->getImageManipulator()) {
             $ext = $this->container->get('liip_imagine.twig.extension'); 
             return call_user_func_array(array($ext, "filter"), $params);
         }
         
-        if ('avalanche_imagine' === $this->getThumbnailGenerator()) {
+        if ('avalanche_imagine' === $this->getImageManipulator()) {
             $ext = $this->container->get('imagine.twig.extension'); 
             return call_user_func_array(array($ext, "applyFilter"), $params);
         }
         
-        return $params[0];
+        // In case no image manipulator is used we
+        // return the unmodified path
+        return $path;
     }
 
     /**
-     * Get thumbnail generator name
+     * Get asset provider name
      *
-     * @return string Parameter value
+     * @return string|null Parameter value
      */
-    public function getThumbnailGenerator()
+    public function getAssetProvider()
+    {
+        return $this->container->getParameter('avocode.form.asset_provider');
+    }
+
+    /**
+     * Get image manipulator name
+     *
+     * @return string|null Parameter value
+     */
+    public function getImageManipulator()
     {
         return $this->container->getParameter('avocode.form.image_manipulator');
     }
