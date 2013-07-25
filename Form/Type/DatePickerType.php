a8f44e8129dcdd8e9f4d3f5566a6554df18e188b
loostro <loostro@gmail.com>
2013-07-25 16:54:08 +0200

    Update DatePickerType.php
    Fix default formating

--- Form/Type/DatePickerType.php
+++ Form/Type/DatePickerType.php
@@ -93,7 +93,7 @@ class DatePickerType extends AbstractType
     {
         $resolver->setDefaults(array(
             'input'                 => 'datetime',
-            'format'                => 'yyyy-mm-dd',
+            'format'                => 'yyyy-MM-dd',
             'week_start'            => 1,
             'calendar_weeks'        => false,
             'start_date'            => date('Y-m-d', strtotime('-20 years')),
