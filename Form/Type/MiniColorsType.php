84611436381ffb747987db3c3cd2e5c3a364676a
loostro <loostro@gmail.com>
2013-08-04 20:21:38 +0200

    Add jQuery Knob form type

--- Form/Type/MiniColorsType.php
+++ Form/Type/MiniColorsType.php
@@ -3,7 +3,6 @@
 namespace Avocode\FormExtensionsBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
