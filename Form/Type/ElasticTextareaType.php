fe1d0c22cee173ecc66dbb3701dee3e02f3c9c3a
Pierrick <pv@webeasybox.com>
2013-08-09 19:42:14 +0200

    add Elastic Textarea Type

---
+++ Form/Type/ElasticTextareaType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Avocode\FormExtensionsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * See `Resources/doc/elastic-textarea/overview.md` for documentation
+ * 
+ * @author Pierrick VIGNAND <pierrick.vignand@gmail.com>
+ */
+class ElasticTextareaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'textarea';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'elastic_textarea';
+    }
+}
