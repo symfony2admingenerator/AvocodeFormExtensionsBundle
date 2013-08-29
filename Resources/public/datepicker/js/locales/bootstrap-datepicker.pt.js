6c93b2d79e22b6357d945e33e5d3b674ef448e7a
Restless-ET <restless86@gmail.com>
2013-08-29 16:53:08 +0100

    Update Portuguese translation for bootstrap-datepicker.

--- Resources/public/datepicker/js/locales/bootstrap-datepicker.pt.js
+++ Resources/public/datepicker/js/locales/bootstrap-datepicker.pt.js
@@ -4,11 +4,13 @@
  * Tiago Melo <tiago.blackcode@gmail.com>
  */
 ;(function($){
-	$.fn.datepicker.dates['pt'] = {
-		days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"],
-		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
-		daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa", "Do"],
-		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
-	};
+    $.fn.datepicker.dates['pt'] = {
+        days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"],
+        daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+        daysMin: ["Do", "Se", "Te", "Qa", "Qi", "Sx", "Sa", "Do"],
+        months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+        monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        today: "Hoje",
+        clear: "Limpar"
+    };
 }(jQuery));
